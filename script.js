f57fb84049e9d16445872a699dc6fd6cba3598e3
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-09 14:15:56 -0500

    project working

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-console.log('javascript sourced');
-
 $(document).ready(readyNow);
 
 function readyNow() {
+  $('.generate').on('click', generateDiv);
+  $('div').on('click', '.swap', swapColors);
+  $('div').on('click', '.delete', deleteBlock);
+}
 
+var generateClicks = 0;
 
+function generateDiv() {
+  generateClicks += 1;
+  $('.blockHolder').append('<div class="block">' +
+    '<p class="counter">' + generateClicks + '</p>' +
+    '<button class="swap">Swap</button>' +
+    '<button class="delete">Delete</button>' + '</div>');
+}
+
+function swapColors() {
+  $(this).parent().css('background-color', 'yellow');
+}
+
+function deleteBlock() {
+  $(this).parent().remove();
 }
